2633acde2eec69edbadd92326b6e6d8d5f77a90c
yshay <yshayy@gmail.com>
2017-01-25 12:34:45 +0200

    first commit

---
+++ Tweek.JPad/AssemblyInfo.fs
@@ -0,0 +1,35 @@
+﻿namespace Tweek.JPad.AssemblyInfo
+
+open System.Resources
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("Tweek.JPad")>]
+[<assembly: AssemblyDescription("JPad Rules Parser")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("Soluto")>]
+[<assembly: AssemblyProduct("Tweek.JPad")>]
+[<assembly: AssemblyCopyright("Copyright ©  2017")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+[<assembly: NeutralResourcesLanguage("en")>]
+
+// Version information for an assembly consists of the following four values:
+// 
+//       Major Version
+//       Minor Version 
+//       Build Number
+//       Revision
+// 
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("0.3.0.0")>]
+[<assembly: AssemblyFileVersion("0.3.0.0")>]
+
+do
+    ()
