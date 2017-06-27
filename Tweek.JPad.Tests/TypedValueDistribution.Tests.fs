fcb300fa84e9140b43b19a730d6ec6db7eec6160
Nataly Shrits <nataly@soluto.com>
2017-06-27 17:45:16 +0300

    added unit tests

--- Tweek.JPad.Tests/TypedValueDistribution.Tests.fs
+++ Tweek.JPad.Tests/TypedValueDistribution.Tests.fs
@@ -21,23 +21,23 @@ type ``TypedValueDistribution Tests`` ()=
     member test.``Use valueType with number typed weighted value distribution``() =
         let rules = parser.Parse """
         {
-    	    "partitions": [],
+            "partitions": [],
             "valueType": "number",
-    	    "rules":[
-                        {
-                            "Id" : "678107bb-51de-46ee-b127-2672c2303a47",
-    			            "Matcher": {},
-                            "Type": "MultiVariant",
-                            "OwnerType": "device",
-                            "ValueDistribution":{
-                                "type" : "weighted",
-                                "args" : {
-                                    "5" : 0,
-                                    "6" : 100
-                                }
-                            }
-    		            }
-                    ]
+            "rules": [
+                {
+                    "Salt": "678107bb-51de-46ee-b127-2672c2303a47",
+                    "Matcher": {},
+                    "Type": "MultiVariant",
+                    "OwnerType": "device",
+                    "ValueDistribution": {
+                        "type": "weighted",
+                        "args": {
+                            "5": 0,
+                            "6": 100
+                        }
+                    }
+                }
+            ]
         }"""
         validate rules context (JsonValue.Number 6M)
 
@@ -45,46 +45,81 @@ type ``TypedValueDistribution Tests`` ()=
     member test.``Use valueType with boolean typed weighted value distribution``() =
         let rules = parser.Parse """
         {
-    	    "partitions": [],
+            "partitions": [],
             "valueType": "boolean",
-    	    "rules":[
-                        {
-                            "Id" : "123",
-    			            "Matcher": {},
-                            "Type": "MultiVariant",
-                            "OwnerType": "device",
-                            "ValueDistribution":{
-                                "type" : "weighted",
-                                "args" : {
-                                    "false" : 0,
-                                    "true" : 100
-                                }
-                            }
-    		            }
-                    ]
+            "rules": [
+                {
+                    "Id": "123",
+                    "Matcher": {},
+                    "Type": "MultiVariant",
+                    "OwnerType": "device",
+                    "ValueDistribution": {
+                        "type": "weighted",
+                        "args": {
+                            "false": 0,
+                            "true": 100
+                        }
+                    }
+                }
+            ]
         }"""
         validate rules context (JsonValue.Boolean true) 
      
-     [<Fact>]
-     member test.``Use valueType with string typed weighted value distribution``() =
-         let rules = parser.Parse """
-         {
-     	    "partitions": [],
-             "valueType": "string",
-     	    "rules":[
-                         {
-                             "Id" : "123",
-     			            "Matcher": {},
-                             "Type": "MultiVariant",
-                             "OwnerType": "device",
-                             "ValueDistribution":{
-                                 "type" : "weighted",
-                                 "args" : {
-                                     "false" : 0,
-                                     "true" : 100
-                                 }
-                             }
-     		            }
-                     ]
-         }"""
-         validate rules context (JsonValue.String "true")
+    [<Fact>]
+    member test.``Use valueType with string typed weighted value distribution``() =
+        let rules = parser.Parse """
+        {
+            "partitions": [],
+            "valueType": "string",
+            "rules": [
+                {
+                    "Id": "123",
+                    "Matcher": {},
+                    "Type": "MultiVariant",
+                    "OwnerType": "device",
+                    "ValueDistribution": {
+                        "type": "weighted",
+                        "args": {
+                            "false" : 0,
+                            "true" : 100
+                        }
+                    }
+                }
+            ]
+        }"""
+        validate rules context (JsonValue.String "true")
+     
+    [<Fact>]
+    member test.``value distribution should prefer Salt over Id``() =
+        let rules = parser.Parse """
+        {
+            "partitions": [],
+            "valueType": "string",
+            "rules": [
+                {
+                    "Salt": "32123",
+                    "Id": "123",
+                    "Matcher": {},
+                    "Type": "MultiVariant",
+                    "OwnerType": "device",
+                    "ValueDistribution": {
+                        "type": "weighted",
+                        "args": {
+                            "false" : 50,
+                            "true" : 50
+                        }
+                    }
+                }
+            ]
+        }"""
+        validate rules context (JsonValue.String "true")
+
+    [<Fact>]
+    member test.``value distribution with Salt should be the same as Id``() =
+        let getRules prop salt = sprintf """{"partitions":[],"valueType":"string","rules":[{"%s":"%s","Matcher":{},"Type":"MultiVariant","OwnerType":"device","ValueDistribution":{"type":"weighted","args":{"false":50,"true":50}}}]}""" prop salt
+        let salt = Guid.NewGuid().ToString()
+
+        let saltRules = getRules "Salt" salt |> parser.Parse
+        let idRules = getRules "Id" salt |> parser.Parse
+
+        saltRules.Invoke context |> should equal (idRules.Invoke context)
